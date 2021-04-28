c0d595ade11b011c9473f46a93d8c02d92ecb1c4
12Isabella <81431189+12Isabella@users.noreply.github.com>
2021-04-28 14:00:49 +0200

    moved project to github

---
+++ src/index.js
@@ -0,0 +1,38 @@
+
+function actualDate () {
+let now = new Date ();
+let dayNumber = now.getDay ();
+let days = ["Sunday", "Monday", "Tuesday", "Wedenesday", "Thursday", "Friday", "Saturday"]
+let day = days[dayNumber];
+
+let date = now.getDate ();
+let monthNumber = now.getMonth ();
+let months = ["01.", "02.", "03.", "04.", "05.", "06.", "07.","08.","09.","10.","11.","12."];
+let month = months[monthNumber];
+let year = now.getFullYear();
+
+return `${day}, ${date}.${month}${year}`
+}
+
+
+let dayDate = document.querySelector ("#day-date");
+dayDate.innerHTML = `on ${actualDate()}`
+
+
+function handleSubmit(event){
+event.preventDefault();
+console.log("running function");
+
+let inputPlace = document.querySelector ("#place");
+let inputFormField = document.querySelector ("#input-form-city");
+
+let valuePlace = inputFormField.value;
+inputPlace.innerHTML = valuePlace;
+
+}
+
+
+let inputFormField = document.querySelector("#input-form");
+inputFormField.addEventListener ("submit", handleSubmit);
+
+
